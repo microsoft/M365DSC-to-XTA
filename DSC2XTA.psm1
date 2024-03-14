20d3fb8879befa0748ee67bcc2453da75324bc87
Nik Charlebois <nicharl@microsoft.com>
2024-03-14 07:04:09 -0400

    Initial

---
+++ DSC2XTA.psm1
@@ -0,0 +1,21 @@
+﻿function ConvertFrom-DSCToXTA
+{
+    [CmdletBinding()]
+    [OutputPath([System.String])]
+    param(
+        [Parameter()]
+        [System.String]
+        $Path,
+
+        [Parameter()]
+        [System.String]
+        $Content
+    )
+    $Global:M365DSCSkipDependenciesValidation = $true
+    if ([System.String]::IsNullOrEmpty($Content) -and -not [System.String]::IsNullOrEmpty($Path))
+    {
+        $Content = Get-Content -Path $Path -Raw
+    }
+
+    $parsedContent = ConvertTo-DSCObject -Content $Content
+}
