ff0600e61d1f70c4ee452e071ef714ecb6dcd643
Nik Charlebois <nicharl@microsoft.com>
2024-03-14 13:54:41 -0400

    Initial Release

--- DSC2XTA.psm1
+++ DSC2XTA.psm1
@@ -1,7 +1,7 @@
 ﻿function ConvertFrom-DSCToXTA
 {
     [CmdletBinding()]
-    [OutputPath([System.String])]
+    [OutputType([System.String])]
     param(
         [Parameter()]
         [System.String]
@@ -11,11 +11,46 @@
         [System.String]
         $Content
     )
+    # Initialization - Skip 
     $Global:M365DSCSkipDependenciesValidation = $true
+
+    # Initialization - Load the Mapping Information
+    $mappingPath = Join-Path -Path $PSScriptRoot -ChildPath 'DSC2XTAMappings.psd1' -Resolve
+    $mappings = Import-PowerShellDataFile $mappingPath
+
+    # Initialization - Load the XTA Template
+    $templatePath = Join-Path -Path $PSScriptRoot -ChildPath 'XTATemplate.json' -Resolve
+    $templateContent = Get-Content -Path $templatePath -Raw
+    $template = ConvertFrom-JSON $templateContent
+    
+    # If a path to a file is provided, then get its raw content
     if ([System.String]::IsNullOrEmpty($Content) -and -not [System.String]::IsNullOrEmpty($Path))
     {
         $Content = Get-Content -Path $Path -Raw
     }
 
+    # Use the DSCParser to convert the file's content into an array of
+    # PowerShell objects (Hashtables).
     $parsedContent = ConvertTo-DSCObject -Content $Content
+
+    # Loop through all the resources and convert them to XTA
+    $allResources = @()
+    foreach ($resource in $parsedContent)
+    {
+        $mappedNamespace = $mappings.($resource.ResourceName)
+        if (-not [System.String]::IsNullOrEmpty($mappedNamespace))
+        {
+            $currentResource = @{
+                name = $resource.ResourceInstanceName
+                type = $mappedNamespace
+            }
+
+            $resource.Remove("ResourceInstanceName") | Out-Null
+            $resource.Remove("ResourceName") | Out-Null
+            $currentResource.Add("properties", $resource)
+            $allResources += $currentResource
+        }
+    }
+    $template.Resources = $allResources
+    return (ConvertTo-Json $template -Depth 99)
 }
